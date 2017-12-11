ce8321c6821d698e95adf6c1a80452eac2d1dbd6
Max Davis <maxdavisdesign@gmail.com>
2017-12-10 21:11:34 -0300

    Validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/maxdavis/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/maxdavis/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.maxdavis.cursomc.domain.Cliente;
+import com.maxdavis.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
