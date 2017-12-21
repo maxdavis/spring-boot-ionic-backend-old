690bcb2d8b2cefbdfe6a3e7b9471287f8433cef9
Max Davis <maxdavisdesign@gmail.com>
2017-12-20 22:01:22 -0300

    Implementando autorizacao

--- src/main/java/com/maxdavis/cursomc/config/SecurityConfig.java
+++ src/main/java/com/maxdavis/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.maxdavis.cursomc.security.JWTAuthenticationFilter;
+import com.maxdavis.cursomc.security.JWTAuthorizationFilter;
 import com.maxdavis.cursomc.security.JWTUtil;
 
 @Configuration
@@ -48,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
