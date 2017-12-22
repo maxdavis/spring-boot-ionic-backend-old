f15fba730f64484b09dea326e7eccd55b19589e0
Max Davis <maxdavisdesign@gmail.com>
2017-12-21 21:05:57 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/maxdavis/cursomc/services/PedidoService.java
+++ src/main/java/com/maxdavis/cursomc/services/PedidoService.java
@@ -3,8 +3,12 @@ package com.maxdavis.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.maxdavis.cursomc.domain.Cliente;
 import com.maxdavis.cursomc.domain.ItemPedido;
 import com.maxdavis.cursomc.domain.PagamentoComBoleto;
 import com.maxdavis.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.maxdavis.cursomc.repositories.ItemPedidoRepository;
 import com.maxdavis.cursomc.repositories.PagamentoRepository;
 import com.maxdavis.cursomc.repositories.PedidoRepository;
 import com.maxdavis.cursomc.repositories.ProdutoRepository;
+import com.maxdavis.cursomc.security.UserSpringSecurity;
+import com.maxdavis.cursomc.services.exceptions.AuthorizationException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -22,22 +28,22 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EmailService emailService;
 
@@ -45,37 +51,46 @@ public class PedidoService {
 		Pedido pedido = pedidoRepository.findOne(id);
 
 		if (pedido == null) {
-			throw new ObjectNotFoundException(
-					"Objeto não encontrado! Id:" + id + ", Tipo:" + Pedido.class.getName());
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo:" + Pedido.class.getName());
 		}
 
 		return pedido;
 	}
-	
+
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
 		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto ) {
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-		
+
 		obj = pedidoRepository.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		
+
 		for (ItemPedido item : obj.getItens()) {
 			item.setDesconto(0.0);
 			item.setProduto(produtoRepository.findOne(item.getProduto().getId()));
 			item.setPreco(item.getProduto().getPreco());
-		    item.setPedido(obj);
+			item.setPedido(obj);
 		}
-		
+
 		itemPedidoRepository.save(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSpringSecurity user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 
 }
